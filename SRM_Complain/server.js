d4eac03d9a20f404a8c9463da17da35cbe4e8c63
Himanshu Kumar <himanshu.kumar133@gmail.com>
2016-05-19 18:23:05 +0530

    Console Update

--- SRM_Complain/server.js
+++ SRM_Complain/server.js
@@ -73,6 +73,7 @@ app.put('/srmcomplain',function(req,res){
 app.delete('/srmcomplain',function(req,res){
 	var complainid = req.body.complainid;
 	complainid = parseInt(complainid);
+	console.log(complainid);
 	if(complainid){
 		connection.query("DELETE from complainrecord WHERE complainId=?",[complainid],function(err, rows, fields){
 			if(!!err){
